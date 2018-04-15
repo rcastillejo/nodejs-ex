b82ddba38ad83ead8ff4b8b48eb130aa7306fdfd
Ricardo Castillejo <ricardo.castillejo@gmail.com>
2018-04-15 15:51:25 -0500

    Agregando logs de conexion

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
+    
+console.log('Connection port: %s', port);
+console.log('Connection ip: %s', ip);
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
@@ -20,6 +23,12 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
       mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
       mongoUser = process.env[mongoServiceName + '_USER'];
+	  
+   console.log('Connection mongoServiceName: %s', mongoServiceName);
+   console.log('Connection mongoPort: %s', mongoPort);
+   console.log('Connection mongoDatabase: %s', mongoDatabase);
+   console.log('Connection mongoPassword: %s', mongoPassword);
+   console.log('Connection mongoUser: %s', mongoUser);
 
   if (mongoHost && mongoPort && mongoDatabase) {
     mongoURLLabel = mongoURL = 'mongodb://';
@@ -32,6 +41,10 @@ if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
 
   }
 }
+
+console.log('Connection mongoURL: %s', mongoURL);
+console.log('Connection mongoURLLabel: %s', mongoURLLabel);
+
 var db = null,
     dbDetails = new Object();
 
@@ -60,7 +73,9 @@ app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    initDb(function(err){
+	  console.log('[/] Error connecting to Mongo. Message:\n'+err);
+	});
   }
   if (db) {
     var col = db.collection('counts');
@@ -81,7 +96,9 @@ app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
   if (!db) {
-    initDb(function(err){});
+    initDb(function(err){
+	  console.log('[/pagecount] Error connecting to Mongo. Message:\n'+err);
+    });
   }
   if (db) {
     db.collection('counts').count(function(err, count ){
